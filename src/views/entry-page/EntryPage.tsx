e20989e98af4a97b8048ba693a765ea893bcc523
DQCoding1 <diegoabquintero@gmail.com>
2023-02-25 23:37:33 -0500

    creation of entry page

---
+++ src/views/entry-page/EntryPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./EntryPage.css";
+
+const EntryPage = () => {
+  return (
+    <section className="
+      section-entry
+      d-flex flex-column justify-content-center align-items-center gap-5
+      w-100 min-vh-100 p-4
+      bg-dark text-white"
+    >
+      <h1 className="fw-bold">Guitar Chords Test</h1>
+      <p className="fs-4 text-center">
+        Test your musical skills recognizing correctly guitar chords
+      </p>
+      <Link to="" className="btn btn-primary px-3">
+        Listen to examples
+      </Link>
+      <Link to="" className="btn btn-primary px-5">
+        Start Test
+      </Link>
+    </section>
+  );
+};
+
+export default EntryPage;
