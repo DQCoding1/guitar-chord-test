24bb2a5f2b3ef1dc1c932193ded81b629c951a64
DQCoding1 <diegoabquintero@gmail.com>
2023-02-28 16:33:21 -0500

    creation of the page listen to examples

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import './App.css';
 import EntryPage from './views/entry-page/EntryPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { routes } from './routes/routes';
+import ListenToExamples from './views/listen-to-examples/ListenToExamples';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={routes.ENTRY} element={<EntryPage />} />
+        <Route path={routes.EXAMPLES} element={<ListenToExamples />} />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
--- src/views/entry-page/EntryPage.tsx
+++ src/views/entry-page/EntryPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { routes } from "../../routes/routes";
 import "./EntryPage.css";
 
 const EntryPage = () => {
@@ -7,14 +8,13 @@ const EntryPage = () => {
     <section className="
       section-entry
       d-flex flex-column justify-content-center align-items-center gap-5
-      w-100 min-vh-100 p-4
-      bg-dark text-white"
+      w-100 min-vh-100 p-4"
     >
       <h1 className="fw-bold">Guitar Chords Test</h1>
       <p className="fs-4 text-center">
         Test your musical skills recognizing correctly guitar chords
       </p>
-      <Link to="" className="btn btn-primary px-3">
+      <Link to={routes.EXAMPLES} className="btn btn-primary px-3">
         Listen to examples
       </Link>
       <Link to="" className="btn btn-primary px-5">
